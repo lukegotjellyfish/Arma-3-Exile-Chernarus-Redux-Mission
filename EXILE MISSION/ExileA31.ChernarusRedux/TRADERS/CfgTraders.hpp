db72f7e7222998e834e11b7284a12597fbf62b91
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-06-29 19:14:27 +0100

    Newest Working Mission

---
+++ EXILE MISSION/ExileA31.ChernarusRedux/TRADERS/CfgTraders.hpp
@@ -0,0 +1,234 @@
+	/**
+	 * Weapons, scopes, silencers, ammo
+	 */
+	class Exile_Trader_Armory
+	{
+		name = "ARMORY";
+		showWeaponFilter = 1;
+		categories[] =
+		{
+			"AssaultRifles",
+			"Pistols",
+			"Shotguns",
+			"SubMachineGuns",
+			"LightMachineGuns",
+			"SniperRifles",
+			"BipodAttachments",
+			"MuzzleAttachments",
+			"OpticAttachments",
+			"PointerAttachments",
+			"Ammunition",
+			"CUPAmmunition",
+			"CUPAssaultRifles",
+			"CUPLightMachineGuns",
+			"CUPAttachments",
+			"CUPBipodAttachments",
+			"CUPMuzzleAttachments",
+			"CUPOpticAttachments",
+			"CUPPistols",
+			"CUPPointerAttachments",
+			"CUPSniperRifles",
+			"CUPSubMachineGuns",
+			"CUPRangefinders",
+			"GREFAmmunition",
+			"GREFWeapons",
+			"NIAAssaultRifles",
+			"NIALightMachineGuns",
+			"NIASniperRifles",
+			"NIAAttachments",
+			"NIAMuzzleAttachments",
+			"NIAOpticAttachments",
+			"NIAAmmunition",
+			"RHSAssaultRifles",
+			"RHSPistols",
+			"RHSSubMachineGuns",
+			"RHSLightMachineGuns",
+			"RHSSniperRifles",
+			"RHSBipodAttachments",
+			"RHSForegripAttachments",
+			"RHSMuzzleAttachments",
+			"RHSOpticAttachments",
+			"RHSPointerAttachments",
+			"RHSAmmunition"
+		};
+	};
+
+	/**
+	 * Satchels, nades, UAVs, static MGs
+	 */
+	class Exile_Trader_SpecialOperations
+	{
+		name = "SPECIAL OPERATIONS";
+		showWeaponFilter = 1; // for noob tubes
+		categories[] =
+		{
+			"A3LauncherAmmo",
+			"A3Launchers",
+			"APEXLauncherAmmo",
+			"ApexLaunchers",
+			"CUPExplosive",
+			"CUPLauncherAmmo",
+			"CUPLaunchers",
+			"CUPNVGs",
+			"ExplosiveWeapons",
+			"Explosives",
+			"Flares",
+			"Navigation",
+			"RHSExplosives",
+			"RHSLauncherAmmunition",
+			"RHSLaunchers",
+			//"RHSStaticMG",		//not recommended except for militarised servers due to rockets/mortar etc
+			"RHSUGLAmmo",
+			"SAFMines",
+			"Smokes",
+			"StaticMGs",
+			"TankDLCLaunchers",
+			"TankDLCLauncherAmmo",
+			"UAVs"
+		};
+	};
+
+	/**
+	 * Uniforms, vests, helmets, backpacks
+	 */
+	class Exile_Trader_Equipment
+	{
+		name = "EQUIPMENT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Backpacks",
+			"Backpacks",
+			"CUPBackpacks",
+			"CUPHeadgear",
+			"CUPUniforms",
+			"CUPVests",
+			"FirstAid",
+			"GREFHeadgear",
+			"GREFUniforms",
+			"GREFVests",
+			"Headgear",
+			"Nlunit",
+			"RHSAccessories",
+			"RHSBinoculars",
+			"RHSBackpacks",
+			"RHSHeadgear",
+			"RHSNightVisonGoggles",
+			"RHSUniforms",
+			"RHSVests",
+			"TankDLCclothing",
+			"Tools",
+			"Uniforms",
+			"Vests"
+		};
+	};
+
+	/**
+	 * Cans, cans, cans
+	 */
+	class Exile_Trader_Food
+	{
+		name = "FAST FOOD";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Drinks",
+			"Food",
+			"NonVeganFood"
+		};
+	};
+
+	/**
+	 * Light bulbs, metal, etc.
+	 */
+	class Exile_Trader_Hardware
+	{
+		name = "HARDWARE";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Hardware",
+			//"R3FTools",
+			"ExtendedBaseMod",
+			"CBuilding"
+		};
+	};
+
+	/**
+	 * Sells cars and general vehicles
+	 */
+	class Exile_Trader_Vehicle
+	{
+		name = "VEHICLE";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Armed",
+			"A3Cars",
+			"A3Trucks",
+			"A3Tanks",
+			"Bikes",
+			"CUPArmed",
+			"CUPBikes",
+			"CUPUnarmed",
+			"Cars",
+			"RHSVehicles",
+			"TankDLC",
+			"Trucks"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	/**
+	 * Sells choppers and planes
+	 */
+	class Exile_Trader_Aircraft
+	{
+		name = "AIRCRAFT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Planes",
+			"A3armedChoppers",
+			"A3unarmedChoppers",
+			"CChoppers",
+			"CPlanes",
+			"CUPChoppers",
+			"CUPPlanes",
+			"Choppers",
+			"Planes",
+			"JetPlanes",
+			"RHSChoppers",
+			"RHSPlanes",
+			"A3UAVs",
+			"CUAVs"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	/**
+	 * Sells ships and boats
+	 */
+	class Exile_Trader_Boat
+	{
+		name = "BOAT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Boats",
+			"Boats",
+			"CUPBoats",
+			"RHSBoats"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	class Exile_Trader_Diving
+	{
+		name = "DIVERS";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Diving"
+		};
+	};
---
+++ EXILE MISSION/ExileA31.ChernarusRedux/TRADERS/TANKDLC/TraderCategoriesTANKDLC.hpp
@@ -0,0 +1,30 @@
+	/////////////////////////////////////////////////////////
+	//	tankDLC submitted by @ItsDutch
+	/////////////////////////////////////////////////////////
+
+    class TankDLC
+	{
+		name = "TANK DLC";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+			"B_AFV_Wheeled_01_cannon_F",
+			"B_AFV_Wheeled_01_up_cannon_F",
+			"B_G_Offroad_01_AT_F",
+			"B_LSV_01_AT_F",
+			"B_T_AFV_Wheeled_01_cannon_F",
+			"B_T_AFV_Wheeled_01_up_cannon_F",
+			"B_T_LSV_01_AT_F",
+			"I_C_Offroad_02_AT_F",
+			"I_LT_01_AA_F",
+			"I_LT_01_AT_F",
+			"I_LT_01_cannon_F",
+			"I_LT_01_scout_F",
+			"O_LSV_02_AT_F",
+			"O_MBT_04_cannon_F",
+			"O_MBT_04_command_F",
+			"O_T_LSV_02_AT_F",
+			"O_T_MBT_04_cannon_F",
+			"O_T_MBT_04_command_F"
+		};
+	};
