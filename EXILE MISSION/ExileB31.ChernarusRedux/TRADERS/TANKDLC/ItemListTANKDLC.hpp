4067dacd834f2e08af122a0a34292ec29eb94b5a
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-06-29 23:19:55 +0100

    Version B (ADK)
    Added ADK trader files, configured cpp for ADK

---
+++ EXILE MISSION/ExileB31.ChernarusRedux/TRADERS/TANKDLC/ItemListTANKDLC.hpp
@@ -0,0 +1,37 @@
+	///////////////////////////////////////////////////////////////////////////////
+	//	tankDLC submitted by @ItsDutch
+	///////////////////////////////////////////////////////////////////////////////
+
+	///////////////////////////////////////////////////////////////////////////////
+	//	RHINO
+	///////////////////////////////////////////////////////////////////////////////
+	class B_AFV_Wheeled_01_cannon_F			{ quality = 1; price = 2000000; };
+	class B_AFV_Wheeled_01_up_cannon_F		{ quality = 1; price = 3000000; };
+	class B_T_AFV_Wheeled_01_cannon_F		{ quality = 1; price = 2000000; };
+	class B_T_AFV_Wheeled_01_up_cannon_F	{ quality = 1; price = 3000000; };
+
+	///////////////////////////////////////////////////////////////////////////////
+	//	awc
+	///////////////////////////////////////////////////////////////////////////////
+	class I_LT_01_AA_F						{ quality = 1; price = 600000; };
+	class I_LT_01_AT_F						{ quality = 1; price = 600000; };
+	class I_LT_01_cannon_F					{ quality = 1; price = 600000; };
+	class I_LT_01_scout_F					{ quality = 1; price = 600000; };
+
+	///////////////////////////////////////////////////////////////////////////////
+	//	T140
+	///////////////////////////////////////////////////////////////////////////////
+	class O_MBT_04_cannon_F					{ quality = 1; price = 6000000; };
+	class O_MBT_04_command_F				{ quality = 1; price = 6500000; };
+	class O_T_MBT_04_cannon_F				{ quality = 1; price = 6000000; };
+	class O_T_MBT_04_command_F				{ quality = 1; price = 6500000; };
+
+	///////////////////////////////////////////////////////////////////////////////
+	//	rocketvehicle
+	///////////////////////////////////////////////////////////////////////////////
+	class B_G_Offroad_01_AT_F				{ quality = 1; price = 250000; };
+	class B_LSV_01_AT_F						{ quality = 1; price = 250000; };
+	class B_T_LSV_01_AT_F					{ quality = 1; price = 250000; };
+	class I_C_Offroad_02_AT_F				{ quality = 1; price = 250000; };
+	class O_LSV_02_AT_F						{ quality = 1; price = 250000; };
+	class O_T_LSV_02_AT_F					{ quality = 1; price = 250000; };
