a9043a898d29c5d6b889dfd8c00d2eb65766c068
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-07-01 22:07:25 +0100

    Making RU-US vehicle categories

---
+++ EXILE MISSION/RHS Vehicle sorting/AFRF classes.hpp
@@ -0,0 +1,70 @@
+	class RHS_SOV-RU_Cars
+	{
+		name = "RHS SOV-RU Cars";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+
+		};
+	};
+	class RHS_SOV-RU_Transport
+	{
+		name = "RHS SOV-RU Transport";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+
+		};
+	};
+	class RHS_SOV-RU_APCs
+	{
+		name = "RHS SOV-RU APCs";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+
+		};
+	};
+	class RHS_SOV-RU_IFVs
+	{
+		name = "RHS SOV-RU IFVs";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+
+		};
+	};
+	class RHS_SOV-RU_Tanks
+	{
+		name = "RHS SOV-RU Tanks";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+			//2S25
+			class rhs_sprut_vdv                                 { quality = 2S25Quality; price = 2S25Price; };  //2S25
+		};
+	};
+	class RHS_SOV-RU_SPAA
+	{
+		name = "RHS SOV-RU SPAA";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+
+		};
+	};
+	class RHS_SOV-RU_Artillery
+	{
+		name = "RHS SOV-RU Artillery";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+			//2S1
+			class rhs_2s1_tv                                    { quality = 2S1Quality; price = 2S1Price; };  //2S1
+			class rhs_2s1_vmf                                   { quality = 2S1Quality; price = 2S1Price; };  //2S1
+			class rhsgref_cdf_2s1                               { quality = 2S1Quality; price = 2S1Price; };  //2S1
+			class rhsgref_cdf_b_2s1                             { quality = 2S1Quality; price = 2S1Price; };  //2S1
+			class rhsgref_ins_2s1                               { quality = 2S1Quality; price = 2S1Price; };  //2S1
+			class rhsgref_ins_g_2s1                             { quality = 2S1Quality; price = 2S1Price; };  //2S1
+		};
+	};
---
+++ EXILE MISSION/RHS Vehicle sorting/USAF classes.hpp
@@ -0,0 +1,63 @@
+	class RHS_US_Cars
+	{
+		name = "RHS US Cars";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+			
+		};
+	};
+	class RHS_US_Transport
+	{
+		name = "RHS US Transport";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};
+	class RHS_US_APCs
+	{
+		name = "RHS US APCs";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};
+	class RHS_US_IFVs
+	{
+		name = "RHS US IFVs";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};
+	class RHS_US_Tanks
+	{
+		name = "RHS US Tanks";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};	
+	class RHS_US_SPAA
+	{
+		name = "RHS US SPAA";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};
+	class RHS_US_Artillery
+	{
+		name = "RHS US Artillery";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] =
+		{
+		
+		};
+	};
